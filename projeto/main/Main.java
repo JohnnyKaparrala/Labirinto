b616c1c72698d7be3c103bef6dca7d89359f46b3
Johnny Kaparrala <30930747+JohnnyKaparrala@users.noreply.github.com>
2017-10-25 20:45:00 -0200

    a

--- projeto/main/Main.java
+++ projeto/main/Main.java
@@ -4,16 +4,11 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-        
->>>>>>> daa1328395fa987ca847f7a54914b7854b3ee305
         try {
             System.out.println("Por favor, digite o nome e o diretorio do arquivo:");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String arquivo = new String(br.readLine());
             
-<<<<<<< HEAD
             File f = new File ( arquivo);
             if (! f.exists()){
                 throw new Exception ("O arquivo nao pode ser encontrado."); 
@@ -24,15 +19,6 @@ public class Main {
             lab.resolva();
             System.out.println(lab.toString());
         }
-=======
-            Labirinto lab = new Labirinto(arquivo);
-            lab.resolva();
-            System.out.println(lab.toString());
-        }
-        catch ( FileNotFoundException erro){
-            System.err.println("O arquivo nao pode ser encontrado.");
-        }
->>>>>>> daa1328395fa987ca847f7a54914b7854b3ee305
         catch(Exception erro) {
             System.err.println(erro);
         }
